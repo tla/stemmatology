5f5e51a5d2ba317f670a5a2eedd10d34b9d51c91
Tara L Andrews <tla@mit.edu>
2011-05-24 15:48:33 +0200

    move scripts where they belong

--- script/svg_from_csv.pl
+++ script/svg_from_csv.pl
@@ -1,14 +1,42 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl
 
 use lib 'lib';
 use strict;
 use warnings;
 use Text::Tradition;
 
-my $collation_graph = Text::Tradition->new( 
-						   'CSV' => $ARGV[0],
-						   'base' => $ARGV[1],
-						   );
+# First: read the base. Make a graph, but also note which
+# nodes represent line beginnings.
 
-print $collation_graph->as_svg();
-print STDERR "Done\n";
+my $tradition = Text::Tradition->new(
+    'CSV' => $ARGV[0],
+    'base' => $ARGV[1],
+    );
+
+
+print $tradition->collation->as_svg();
+print STDERR "DONE\n";
+__END__
+my $rows = 0;
+my $matrix = [];
+foreach my $pos ( $collation_graph->{'positions'}->all ) {
+    my @p_nodes = $collation_graph->{'positions'}->nodes_at_position( $pos );
+    $rows = scalar @p_nodes
+	if $rows < scalar @p_nodes;
+    push( @$matrix, \@p_nodes );
+}
+print "<html><head><title>A table</title></head><body><table>\n";
+foreach my $i ( 0 .. $rows-1 ) {
+    print "\t<tr>\n";
+    foreach my $col( @$matrix ) {
+	my $str = '';
+	if( $col->[$i] ) {
+	    $str = $collation_graph->node( $col->[$i] )->label;
+	}
+	printf( "\t\t<td>%s</td>\n", $str );
+    }
+    print "\t</tr>\n";
+}
+print "</table></body></html>\n";
+    
+
