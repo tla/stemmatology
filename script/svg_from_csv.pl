1f563ac315e46e9e05f82b7c803e35d8a4a2fe14
tla <tla@mit.edu>
2011-05-25 10:56:04 +0200

    add expand/collapse path edge functionality

--- script/svg_from_csv.pl
+++ script/svg_from_csv.pl
@@ -5,10 +5,10 @@ use strict;
 use warnings;
 use Text::Tradition;
 
-my $collation_graph = Text::Tradition->new( 
-						   'CSV' => $ARGV[0],
-						   'base' => $ARGV[1],
-						   );
+my $tradition = Text::Tradition->new( 
+				      'CSV' => $ARGV[0],
+				      'base' => $ARGV[1],
+				      );
 
-print $collation_graph->as_svg();
+print $tradition->collation->as_svg();
 print STDERR "Done\n";
