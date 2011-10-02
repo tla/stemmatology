ce14597feff81761c2f1698c76cc0d3de9a7d4aa
Tara L Andrews <tla@mit.edu>
2011-10-03 01:37:16 +0200

    changes requested by Joris

--- TreeOfTexts/root/js/interaction.js
+++ TreeOfTexts/root/js/interaction.js
@@ -1,5 +1,10 @@
+$(document).ready(function() {
+  $('.rowid').click( function() {
+    $(this).parent().nextAll('.clickable').children('span').click();
+  });
+})
 function color_nodes( arr_node_ids ) {
-  $('ellipse').attr( {stroke:'black', fill:'#fff'} );
+  $('ellipse').attr( {stroke:'black', fill:'#999'} );
   jQuery.each( arr_node_ids, function(index,value) {
     $('.node').children('title').filter( function(index) {
       return $(this).text() == value;
