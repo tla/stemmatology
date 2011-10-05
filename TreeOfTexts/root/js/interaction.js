119157ee32c7556eb1619687c9135a0620a6d267
Joris van Zundert <joris.van.zundert@gmail.com>
2011-10-06 01:24:17 +0200

    Added coloting of polygons.

--- TreeOfTexts/root/js/interaction.js
+++ TreeOfTexts/root/js/interaction.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
   $('.rowid').click( function() {
     row_triggered = true;
     $('ellipse').attr( {stroke:'black', fill:'#fff'} );
+    $('.node').children('polygon').attr( {stroke:'#fff', fill:'#fff'} );
+    $('.node').children('text').attr( {stroke:'none', fill:'#000'} );
     $('tr.active_variant_row').children('td').removeClass('cellb0 cellb1 cellb2 cellb3 cellb4 cellb5 cellb6 cellb7'); 
     $(this).parent().nextAll('.clickable').children('span').click();
     $('td.active_variant_cell').removeClass('active_variant_cell');
@@ -15,27 +17,23 @@ function color_nodes( column_index, arr_node_ids, arr_greynode_ids ) {
     $('tr.active_variant_row').children('td').removeClass('cellb0 cellb1 cellb2 cellb3 cellb4 cellb5 cellb6 cellb7'); 
     $('td.active_variant_cell').removeClass('active_variant_cell');
     $('ellipse').attr( {stroke:'black', fill:'#fff'} );
+    $('.node').children('polygon').attr( {stroke:'#fff', fill:'#fff'} );
+    $('.node').children('text').attr( {stroke:'none', fill:'#000'} );
   }; 
   $('tr.active_variant_row').removeClass('active_variant_row') 
   jQuery.each( arr_greynode_ids, function(index,value) {
-    $('.node').children('title').filter( function(index) {
+    nodes = $('.node').children('title').filter( function(index) {
       return $(this).text() == value;
-    }).siblings('ellipse').each( function( index ) {
-        $(this).attr( {stroke:'black', fill:'#444'} );
+    })
+    nodes.siblings('ellipse, polygon, text').each( function( index ) {
+        $(this).attr( {stroke:'#ddd', fill:'#f8f8f8'} );
       });
   });
   jQuery.each( arr_node_ids, function(index,value) {
     $('.node').children('title').filter( function(index) {
       return $(this).text() == value;
-    }).siblings('ellipse').each( function( index ) {
+    }).siblings('ellipse, polygon').each( function( index ) {
         $(this).attr( {stroke:'black', fill:colors[column_index-1]} );
       });
   });
 }
-
-// 8 columns tied to colors, based on td position I guess
-// click on row id: all colors on, remaining (unused) nodes greyed
-// click on cell: those colors on, unused nodes greyed
-// c
-
-// 
