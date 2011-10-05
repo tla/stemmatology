d5c17cec9d1757bebcc71ca5834029b140435363
Joris van Zundert <joris.van.zundert@gmail.com>
2011-10-05 22:51:31 +0200

    Change interactive color coding.

--- TreeOfTexts/root/js/interaction.js
+++ TreeOfTexts/root/js/interaction.js
@@ -4,15 +4,19 @@ $(document).ready(function() {
   $('.rowid').click( function() {
     row_triggered = true;
     $('ellipse').attr( {stroke:'black', fill:'#fff'} );
+    $('tr.active_variant_row').children('td').removeClass('cellb0 cellb1 cellb2 cellb3 cellb4 cellb5 cellb6 cellb7'); 
     $(this).parent().nextAll('.clickable').children('span').click();
-    $('tr.active_variant_row').find('td').removeClass( 'active_variant_cell' );
+    $('td.active_variant_cell').removeClass('active_variant_cell');
     row_triggered = false;
   });
 })
 function color_nodes( column_index, arr_node_ids, arr_greynode_ids ) {
-  if( !row_triggered ) { 
-    $('ellipse').attr( {stroke:'black', fill:'#fff'} ) 
+  if( !row_triggered ) {
+    $('tr.active_variant_row').children('td').removeClass('cellb0 cellb1 cellb2 cellb3 cellb4 cellb5 cellb6 cellb7'); 
+    $('td.active_variant_cell').removeClass('active_variant_cell');
+    $('ellipse').attr( {stroke:'black', fill:'#fff'} );
   }; 
+  $('tr.active_variant_row').removeClass('active_variant_row') 
   jQuery.each( arr_greynode_ids, function(index,value) {
     $('.node').children('title').filter( function(index) {
       return $(this).text() == value;
