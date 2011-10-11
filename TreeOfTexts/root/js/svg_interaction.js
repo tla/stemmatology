8145af1afbb878e5b06d7dc0b69c2a1678a775f0
Joris van Zundert <joris.van.zundert@gmail.com>
2011-10-11 12:58:04 +0200

    Broke it, mend it please.

--- TreeOfTexts/root/js/svg_interaction.js
+++ TreeOfTexts/root/js/svg_interaction.js
@@ -4,30 +4,7 @@ $(document).ready(function() {
   $('svg').width('485px');
 })
 
-function de_color_nodes() {
-    $('ellipse').attr( {stroke:'black', fill:'#fff'} );
-    $('.node').children('polygon').attr( {stroke:'#fff', fill:'#fff'} );
-    $('.node').children('text').attr( {stroke:'none', fill:'#000'} );
-}
 
-function do_color_nodes( topic, data, subscriberData ) {
- // data = {column_index, arr_node_ids, arr_greynode_ids}
-    jQuery.each( data.arr_greynode_ids, function(index,value) {
-      nodes = $('.node').children('title').filter( function(index) {
-        return $(this).text() == value;
-      })
-      nodes.siblings('ellipse, polygon, text').each( function( index ) {
-          $(this).attr( {stroke:'#ddd', fill:'#f8f8f8'} );
-        });
-    });
-    jQuery.each( data.arr_node_ids, function(index,value) {
-      $('.node').children('title').filter( function(index) {
-        return $(this).text() == value;
-      }).siblings('ellipse').each( function( index ) {
-          $(this).attr( {stroke:'black', fill:colors[data.column_index-1]} );
-        });
-    });
-}
 
 function load_stemma_svg( topic, data, subscriberData ) {
     var params = {};
--- TreeOfTexts/root/js/table_interaction.js
+++ TreeOfTexts/root/js/table_interaction.js
@@ -1,15 +1,5 @@
 var colors = ['#ffeeaa','#afc6e9','#d5fff6','#ffccaa','#ffaaaa','#e5ff80','#e5d5ff','#ffd5e5'];
 var row_triggered = false;
-$(document).ready(function() {
-  $('.rowid').click( function() {
-    row_triggered = true;
-    de_color_nodes();
-    $('tr.active_variant_row').children('td').removeClass('cellb0 cellb1 cellb2 cellb3 cellb4 cellb5 cellb6 cellb7'); 
-    $(this).parent().nextAll('.clickable').children('span').click();
-    $('td.active_variant_cell').removeClass('active_variant_cell');
-    row_triggered = false;
-  });
-})
 
 function color_nodes( column_index, arr_node_ids, arr_greynode_ids ) {
   if( !row_triggered ) {
