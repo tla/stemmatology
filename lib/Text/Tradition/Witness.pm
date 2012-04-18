de8fad89208060d3d735b48c9e51735f5a63f977
tla <tla@mit.edu>
2012-04-18 12:15:11 +0200

    remove deprecated source attribute

--- lib/Text/Tradition/Witness.pm
+++ lib/Text/Tradition/Witness.pm
@@ -250,13 +250,6 @@ has 'sourcetype' => (
 	required => 1, 
 );
 
-has 'source' => (
-	is => 'ro',
-	isa => 'Str',
-	predicate => 'has_old_source',
-	clearer => 'clear_old_source',
-);
-
 has 'file' => (
 	is => 'ro',
 	isa => 'Str',
