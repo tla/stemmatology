6795f2701800be5baf9a260ee38ce4f4a1c1c2d6
Tara L Andrews <tla@mit.edu>
2012-09-14 14:05:21 +0200

    make test DB generation script work with new world order

--- base/script/maketestdb.sh
+++ base/script/maketestdb.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-( cd stemmaweb/db && rm -f traditions.db.test && rm -f traditions.db && ln -s traditions.db.test traditions.db )
-script/make_tradition.pl -i self -o db -s t/data/florilegium.dot t/data/florilegium_graphml.xml
-script/make_tradition.pl -i self -o db --dbid 'tradition:notre_besoin' -s t/data/besoin.dot t/data/besoin.xml
-script/admin_users.pl -u 'admin@example.org' -p 'AdminPass' -r 'admin'
-script/admin_users.pl -u 'user@example.org' -p 'UserPass'
-script/admin_users.pl -c modify -u 'user@example.org' -t 'tradition:notre_besoin'
+DBDIR=$1
+DSN=dbi:SQLite:dbname=$DBDIR/traditions.db
+
+( cd $DBDIR && rm -f traditions.db.test && rm -f traditions.db && ln -s traditions.db.test traditions.db )
+script/make_tradition.pl -i self -o db --dsn $DSN -s t/data/florilegium.dot t/data/florilegium_graphml.xml
+script/make_tradition.pl -i self -o db --dsn $DSN --dbid 'tradition:notre_besoin' -s t/data/besoin.dot t/data/besoin.xml
+script/admin_users.pl --dsn $DSN -u 'admin@example.org' -p 'AdminPass' -r 'admin'
+script/admin_users.pl --dsn $DSN -u 'user@example.org' -p 'UserPass'
+script/admin_users.pl --dsn $DSN -c modify -u 'user@example.org' -t 'tradition:notre_besoin'
 
